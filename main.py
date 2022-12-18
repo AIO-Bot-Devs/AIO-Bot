c33ba6315eb05103d72b2c7528c7b657f03f4757
EvilPanda <73741879+VeryEvilPanda@users.noreply.github.com>
2022-12-18 16:06:36 +0000

    Command sync flags stuff

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from disnake.ext import commands
 import os
 from dotenv import load_dotenv
 import json
+import aiofiles
 
 
 #Loads the config file
@@ -39,12 +40,14 @@ intents.reactions = True
 config = getConfig()
 
 
+command_sync_flags = commands.CommandSyncFlags.all()
+
 #Define the test guilds if the bot is in dev mode
 if config[1] == True:
     bot = commands.Bot(
     command_prefix=config[3],
     test_guilds=config[2],
-    sync_commands_debug=True,
+    command_sync_flags=command_sync_flags,
     intents=intents,
     activity = disnake.Activity(name=config[5])
 )
@@ -52,7 +55,7 @@ if config[1] == True:
 else:
     bot = commands.Bot(
         command_prefix=config[3],
-        sync_commands_debug=True,
+        command_sync_flags=command_sync_flags,
         intents=intents,
         activity = disnake.Activity(name=config[5])
     )
@@ -70,7 +73,8 @@ bot.emoji_loading = config[9]["loading"]
 # add ownerd id and footer from config to bot
 bot.owner_id = config[0]
 bot.footer = config[7]
-
+bot.status_enum = commands.option_enum({"Online": disnake.Status.online.value, "Idle": disnake.Status.idle.value, "Do Not Disturb": disnake.Status.dnd.value, "Invisible": disnake.Status.invisible.value})
+bot.status_dict = {"online": disnake.Status.online, "idle": disnake.Status.idle, "dnd": disnake.Status.dnd, "invisible": disnake.Status.invisible}
 
 #Adds cogs to the main bot (if they are enabled in config.json)
 cogs = config[10]
@@ -89,26 +93,47 @@ async def on_ready():
     print("------")
     print(f"Logged in as {bot.user}")
     print("------")
-    try:    
-        uptime = await bot.fetch_channel(config[6])
-        if config[1] == True:
-            dev = bot.emoji_check
-        else:
-            dev = bot.emoji_cross
-        cogs_string = ""
-        for i in cogs:
-            cogs_string += "\n"
-            if cogs[i] == True:
-                cogs_string += f">   • {bot.emoji_check} {i} enabled"
+    if config[1] == True:
+        try:    
+            uptime = await bot.fetch_channel(config[6])
+            if config[1] == True:
+                dev = bot.emoji_check
             else:
-                cogs_string += f">   • {bot.emoji_cross} {i} disabled"
-        await uptime.send(f"{bot.emoji_check} **{bot.user.mention} online!**\n> Disnake: {disnake.__version__}\n> Latency: {int(bot.latency * 1000)}ms\n> Dev mode: {dev}\n> Guilds: {len(bot.guilds)}\n> Cogs: {cogs_string}")
-    except:
-        print("Uptime channel not found")
+                dev = bot.emoji_cross
+            cogs_string = ""
+            for i in cogs:
+                cogs_string += "\n"
+                if cogs[i] == True:
+                    cogs_string += f">   • {bot.emoji_check} {i} enabled"
+                else:
+                    cogs_string += f">   • {bot.emoji_cross} {i} disabled"
+            await uptime.send(f"{bot.emoji_check} **{bot.user.mention} online!**\n> Disnake: {disnake.__version__}\n> Latency: {int(bot.latency * 1000)}ms\n> Dev mode: {dev}\n> Guilds: {len(bot.guilds)}\n> Cogs: {cogs_string}")
+        except:
+            print("Uptime channel not found")
+    else:
+        print("No uptime message sent, bot is not in dev mode")
 
 
-        
+@bot.slash_command()
+@commands.is_owner()
+async def admin(inter):
+    pass
 
+@admin.sub_command()
+async def status(inter, status: bot.status_enum):
+    async with aiofiles.open("config.json", "r") as f:
+        data = json.loads(await f.read())
+    activity = disnake.Game(name=data["activity"])
+    data["status"] = status
+    async with aiofiles.open("config.json", "w") as f:
+        await f.write(json.dumps(data, indent=4))
+    await bot.change_presence(status=status, activity=activity)
+    await inter.response.send_message(f"{bot.emoji_check} Status set to {status}")
+    print(f"Status set to {status} by {inter.author.name} ({inter.author.id})")
+
+
+
+    
 
 #Make this at some point lol
 # @bot.slash_command()
